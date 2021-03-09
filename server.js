04877504e9618448dea32a9007476676b4085d44
tuanthanh2067 <tuanthanh2067@gmail.com>
2021-03-09 14:03:44 -0500

    update for deploy

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
 
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
